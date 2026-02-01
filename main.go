ffac379541f7affb12736e7da56d5ad50eaf7eb5
Deepanshu Chaid <bantar117@gmail.com>
2026-02-01 17:26:40 +0000

    FILE HANDLING

--- main.go
+++ main.go
@@ -13,18 +13,18 @@ func main() {
 
   // fmt.Println(string(f))
 
-  dir, err := os.Open(".")
-  if err != nil {
-    panic(err)
-  }
+  // dir, err := os.Open(".")
+  // if err != nil {
+  //   panic(err)
+  // }
 
-  defer dir.Close()
+  // defer dir.Close()
 
-  fileInfo, err := dir.Readdir(-1)
+  // fileInfo, err := dir.Readdir(-1)
 
-  for _, file := range fileInfo {
-    fmt.Println(file.Name())
-  }
+  // for _, file := range fileInfo {
+  //   fmt.Println(file.Name())
+  // }
 
 
   // array := []int{1,2,3,4,5,6,7,8,9,10}
@@ -33,4 +33,14 @@ func main() {
   //   fmt.Println(i, v)
   // }
 
+  f, err := os.Create("example1.2.txt")
+  if err != nil {
+    panic(err)
+  }
+  
+  defer f.Close()
+  
+  f.WriteString(" i like ahhhhhhhh wtf my does sukhmeen comes in my fkin mind")
+
+fmt.Println("File created successfully")
 }
