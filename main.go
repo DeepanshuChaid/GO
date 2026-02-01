bbf4bb72fa0d70ae0e3a4db24ac42233ac3d0e27
Deepanshu Chaid <bantar117@gmail.com>
2026-02-01 17:46:41 +0000

    FILE HANDLING DONE

--- main.go
+++ main.go
@@ -3,91 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
-  "bufio"
 )
 
 func main() {
-  // f, err := os.ReadFile("example.txt")
-  // if err != nil {
-  //   panic(err)
-  // }
+  file, err := os.Open("example.txt")
+  if err != nil {
+    fmt.Println(err)
+  }
 
-  // fmt.Println(string(f))
-
-  // dir, err := os.Open(".")
-  // if err != nil {
-  //   panic(err)
-  // }
-
-  // defer dir.Close()
-
-  // fileInfo, err := dir.Readdir(-1)
-
-  // for _, file := range fileInfo {
-  //   fmt.Println(file.Name())
-  // }
-
-
-  // array := []int{1,2,3,4,5,6,7,8,9,10}
-
-  // for i, v := range array {
-  //   fmt.Println(i, v)
-  // }
-
-  f, err := os.Create("example1.2.txt")
+  fileInfo, err := file.Stat()
   if err != nil {
     panic(err)
   }
-  
-  defer f.Close()
-  
-  // f.WriteString(" i like ahhhhhhhh wtf my does sukhmeen comes in my fkin mind")
-
-  // bytes := []byte("i definitely like sukhmeen damn bitch ass ai really said that")
-  // f.Write(bytes)
-
-  fmt.Println("File created successfully")
+  fmt.Println(fileInfo.Size())
 
 
-  sourceFile, err := os.Open("example.txt")
-  if err != nil {
-     panic(err)
-  }
-
-  defer sourceFile.Close()
-
-  destinationFile, err := os.Create("example_copy.txt")
-  if err != nil {
-     panic(err)
-  }
-
-  defer destinationFile.Close()
-
-  reader := bufio.NewReader(sourceFile)
-  writer := bufio.NewWriter(destinationFile)
-
-  for {
-    b, err := reader.ReadByte()
-    if err != nil {
-      if err.Error() == "EOF" {
-        panic(err)
-      }
-
-      break
-    }
-    
-    e := writer.WriteByte(b)
-    if e != nil {
-      panic(e)
-    }
-    
-  }
-
-  writer.Flush()
-
-
-  fmt.Println("File copied successfully")
-  
+  os.Remove("example.txt")
+  os.Remove("example1.2.txt")
+  os.Remove("example_copy.txt")
 }
+
 
 
