9bbccd402718d7c23ae4235a3a0d33b7c18d0c1c
Deepanshu Chaid <bantar117@gmail.com>
2026-02-02 13:48:43 +0000

    packages

--- main.go
+++ main.go
@@ -2,26 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func main() {
-  file, err := os.Open("example.txt")
-  if err != nil {
-    fmt.Println(err)
-  }
+func main (){
+  fmt.Println("Hello World")
 
-  fileInfo, err := file.Stat()
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println(fileInfo.Size())
-
-
-  os.Remove("example.txt")
-  os.Remove("example1.2.txt")
-  os.Remove("example_copy.txt")
+  auth.Login()
 }
-
-
-
