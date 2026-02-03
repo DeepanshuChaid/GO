28255c0518e2b85b843060ee59750269caec3b70
Deepanshu Chaid <bantar117@gmail.com>
2026-02-03 19:24:17 +0000

    server

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
-package main 
+package main
 
 import (
-  "fmt"
-  "github.com/DeepanshuChaid/GO/internal/config"
+	"fmt"
+	"net/http"
+    "log"
+	"github.com/DeepanshuChaid/GO/internal/config"
 )
 
 
 func main () {
 
   cfg := config.MustLoad()
-  
-  
-  fmt.Println("Hello World")
+
+
+  router := http.NewServeMux()
+
+  router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World")
+  })
+
+
+  server := http.Server{
+    Addr: cfg.Address,
+    Handler: router,
+  }
+
+  err := server.ListenAndServe()
+  if err != nil {
+    log.Fatal(err)
+  }
 }
