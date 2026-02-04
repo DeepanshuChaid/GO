2f0be4d653ee193835d46a6f27c3929f349ccac5
Deepanshu Chaid <bantar117@gmail.com>
2026-02-04 18:24:16 +0000

    LEARNING FOLDER MAGNAGEMENT

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,12 +1,25 @@
 package student
 
 import (
-  "net/http"
-  "log/slog"
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/DeepanshuChaid/GO/internal/types"
 )
   
 func New() http.HandlerFunc{
   return func(w http.ResponseWriter, r *http.Request) {
+
+    var student types.Student;
+
+    err := json.NewDecoder(r.Body).Decode(&student)
+    if errors.Is(err, io.EOF) {
+      slog.Error("Error decoding student", slog.String("error", err.Error()))
+      w.WriteHeader(http.StatusBadRequest)
+    }
     
     slog.Info("Creating a student")
     
